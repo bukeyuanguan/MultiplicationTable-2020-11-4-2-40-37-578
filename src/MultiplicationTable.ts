ba124b44495cdefcfe70f4d47695f509d43fd49c
Ge Luxue <lge5@slb.com>
2020-11-18 15:42:29 +0800

    add 2*4

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,56 @@
+
+interface Expression {
+  factor1: number,
+  factor2: number,
+  product: number
+}
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
+
+  public render(start: number, end: number): string {
+    if (!this.isBetween1To10(start)) return 'the start number should between 1 and 10'
+    if (!this.isBetween1To10(end)) return 'the end number should between 1 and 10'
+    if (!this.isEndGreaterThanStart(start, end)) return ''
+    const expressions = this.getExpressions(start, end)
+    const getFormatString = this.getFormatString(expressions)
+    return getFormatString
+  }
+  private isEndGreaterThanStart(start: number, end: number): boolean {
+    return start <= end
+  }
+  private isBetween1To10(input: number): boolean {
+    return (input >= 1) && (input <= 10)
+  }
+
+  private getExpressions(start: number, end: number): Expression[][] {
+    const output: Expression[][] = []
+    for (let i = start; i <= end; i++) {
+      const column: Expression[] = []
+      for (let j = start; j <= start + i; j++) {
+        const element: Expression = {
+          factor1: j,
+          factor2: i,
+          product: i * j
+        }
+        column.push(element)
+      }
+      output.push(column)
+    }
+    return output
+    //console.log(output)
+  }
+
+  private getFormatString(data: Expression[][]): string {
+    let outputString = ''
+    for (let i = 0; i < data.length; i++) {
+      let columnString = ''
+      for (let j = 0; j <= i; j++) {
+        let elementString = `${data[i][j].factor1}*${data[i][j].factor2}=${data[i][j].product}`
+        if (!(i === j)) { elementString += '  ' }
+        columnString += elementString
+      }
+      if (!(i === data.length - 1)) { columnString += '\n' }
+      outputString += columnString
+    }
+    return outputString
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -4,13 +4,51 @@ describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
     // given
     const table = new MultiplicationTable()
-    const start = 1
-    const end = 1
+    const start = 2
+    const end = 4
 
     //when
     const rendered = table.render(start, end)
 
     //then
-    expect(rendered).toBe('1*1=1')
+    expect(rendered).toBe('2*2=4\n2*3=6  3*3=9\n2*4=8  3*4=12  4*4=16')
+  })
+
+  it('should start in the range between 1 to 10', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 0
+    const end = 11
+
+    //when
+    const rendered1 = table.render(start, end)
+
+    //then
+    expect(rendered1).toBe('the start number should between 1 and 10')
+  })
+  it('should end in the range between 1 to 10', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 3
+    const end = 20
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('the end number should between 1 and 10')
+  })
+
+  it('should end greater than start', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 3
+    const end = 2
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
   })
 })
